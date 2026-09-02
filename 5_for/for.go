1e1a35f27fa27fe697db4f4d9e11b8a25cef4174
almamun528 <ayapths@gmail.com>
2026-09-03 00:01:30 +0600

    for loop with range

---
+++ 5_for/for.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// for --> only construct into go for looping
+func main() {
+	i := 0
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// this is an infinity loop
+	// for {
+	// 	fmt.Println("run ")
+	// }
+
+	//? classic for loop
+	for i := 0; i <= 10; i++ {
+		//skip any number
+		if i == 2 {
+			continue
+		}
+		fmt.Println("show", i)
+	}
+	// range
+	fmt.Printf("_____range________")
+	// range is similar like for of loop into js which reads into 1st to last each element
+	for i := range 5 {
+		fmt.Println(i)
+	}
+}

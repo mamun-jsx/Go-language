c9d30e6b877edfb83b408cb5614803cd34f9fcab
almamun528 <ayapths@gmail.com>
2026-09-02 23:46:33 +0600

    how to use constant

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// unchangeable  variable we can declare it outside of main func
+	//  but  if you use short hand name:="my name" it will not work
 	const name string = "Go Lang"
 	fmt.Println(name)
+	// ! multiple constant
+	const (
+		userName = "School-management"
+		host     = "localhost"
+		port     = 4001
+	)
+	fmt.Println(userName,port,host)
 }

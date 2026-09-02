484f4b137ec9494178e4532db1890a94b3115a33
almamun528 <ayapths@gmail.com>
2026-09-02 23:39:37 +0600

    add variables

---
+++ 1_hello_world/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // this is the packaged
+
+func main() { // main function
+	fmt.Println("hello world")
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main() {
+	// string
+	var name string = "name"
+	fmt.Println(name)
+	// bool
+	var isAdult bool = true
+	fmt.Println(isAdult)
+	// int
+	var age int = 30
+	fmt.Println(age)
+
+	// short hand syntax
+	lName := "Last name"
+	fmt.Println(lName)
+}
